94ff644e537f75a1c278aaccc2856144b479d190
Rekaterina <rkatherine0602@gmail.com>
2019-10-11 19:59:20 +0300

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+      this.minValue = null;
+      this.maxValue = null;
     }
-
+  
+    setRange(minValue, maxValue) {
+      this.minValue = minValue;
+      this.maxValue = maxValue;
+    }
+  
     guess() {
-
+      return Math.ceil((this.minValue + this.maxValue) / 2);
     }
-
+  
     lower() {
-
+      this.maxValue = this.guess();
     }
-
+  
     greater() {
-
+      this.minValue = this.guess();
     }
-}
+  }
+  
 
 module.exports = GuessingGame;
